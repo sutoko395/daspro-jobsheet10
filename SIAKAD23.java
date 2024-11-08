478d163a1c6bfaabcfeeac1b35879c6e6d7d7dda
sutoko395 <sutokotokotoko@gmail.com>
2024-11-08 09:44:24 +0700

    PUSH SIAKAD23

--- SIAKAD23.java
+++ SIAKAD23.java
@@ -1,33 +1,41 @@
 import java.util.Scanner;
+
 public class SIAKAD23 {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int [][] nilai = new int[4][3];
+        System.out.print("Masukkan jumlah siswa: ");
+        int jumlahSiswa = sc.nextInt();
+        System.out.print("Masukkan jumlah mata kuliah: ");
+        int jumlahMatkul = sc.nextInt();
 
-        for(int i=0; i<nilai.length; i++) {
-            System.out.println("Input nilai mahasiswa ke-" + (i+1));
+        int[][] nilai = new int[jumlahSiswa][jumlahMatkul];
+
+        for (int i = 0; i < jumlahSiswa; i++) {
+            System.out.println("Input nilai mahasiswa ke-" + (i + 1));
             double totalPerSiswa = 0;
 
-            for(int j=0; j<nilai[i].length; j++) {
-                System.out.print("Nilai mata kuliah " + (j+1) + ": ");
+            for (int j = 0; j < jumlahMatkul; j++) {
+                System.out.print("Nilai mata kuliah " + (j + 1) + ": ");
                 nilai[i][j] = sc.nextInt();
                 totalPerSiswa += nilai[i][j];
             }
-            System.out.println("Nilai rata-rata: " + totalPerSiswa/3);
+            System.out.println("Nilai rata-rata: " + totalPerSiswa / jumlahMatkul);
         }
 
         System.out.println("\n===================================");
         System.out.println("Rata-rata Nilai setiap Mata Kuliah:");
 
-        for(int j=0; j<3; j++) {
+        for (int j = 0; j < jumlahMatkul; j++) {
             double totalPerMatkul = 0;
 
-            for(int i=0; i<4; i++) {
+            for (int i = 0; i < jumlahSiswa; i++) {
                 totalPerMatkul += nilai[i][j];
             }
-            System.out.println("Mata Kuliah" + (j+1) + ": " +totalPerMatkul / 4);
+            System.out.println("Mata Kuliah " + (j + 1) + ": " + totalPerMatkul / jumlahSiswa);
         }
+
+        sc.close();
     }
 }
