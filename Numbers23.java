751d2a38591cd87f5a7e9ca2dd15fedf01a8323c
sutoko395 <sutokotokotoko@gmail.com>
2024-11-08 09:41:20 +0700

    PUSH NUMBER23

--- Numbers23.java
+++ Numbers23.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class Numbers23 {
 
     public static void main(String[] args) {
@@ -6,5 +8,12 @@ public class Numbers23 {
         myNumbers[0] = new int[5];
         myNumbers[1] = new int[3];
         myNumbers[2] = new int[1];
+
+        for(int i=0; i<myNumbers.length; i++) {
+            System.out.println(Arrays.toString(myNumbers[i]));
+        }
+        for(int i=0; i<myNumbers.length; i++){
+            System.out.println("Panjang baris ke-" + (i+1) + ": " + myNumbers[i].length);
+        }
     }
 }
